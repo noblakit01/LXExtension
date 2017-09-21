8b420be7763909303bdbd4be68940bf7ee6ba32e
noblakit01 <noblakit01@gmail.com>
2017-09-21 22:53:02 +0700

    add extension crop image

--- Sources/UIImage.swift
+++ Sources/UIImage.swift
@@ -18,4 +18,23 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return image
     }
+    
+    func cropByRect(_ rect: CGRect) -> UIImage? {
+        let rectTransform : CGAffineTransform
+        switch (imageOrientation) {
+        case .left:
+            rectTransform = CGAffineTransform(rotationAngle: CGFloat(90.0).radians).translatedBy(x: 0, y: -size.height)
+        case .right:
+            rectTransform = CGAffineTransform(rotationAngle: CGFloat(-90).radians).translatedBy(x: -size.width, y: 0)
+        case .down:
+            rectTransform = CGAffineTransform(rotationAngle: CGFloat(-180).radians).translatedBy(x: -size.width, y: -size.height)
+        default:
+            rectTransform = CGAffineTransform.identity
+        }
+        
+        let transformedCropSquare = rect.applying(rectTransform)
+        let imageRef: CGImage? = cgImage!.cropping(to: transformedCropSquare)
+        
+        return (imageRef != nil) ? UIImage(cgImage: imageRef!, scale: 1.0, orientation: imageOrientation): nil
+    }
 }

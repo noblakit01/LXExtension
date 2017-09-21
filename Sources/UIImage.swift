7112ab7d0c60cb1f3319ef06019ed1b8f1fe96c1
noblakit01 <noblakit01@gmail.com>
2017-09-21 22:58:19 +0700

    add Resize UIImage

--- Sources/UIImage.swift
+++ Sources/UIImage.swift
@@ -19,7 +19,7 @@ extension UIImage {
         return image
     }
     
-    func cropByRect(_ rect: CGRect) -> UIImage? {
+    func crop(with rect: CGRect) -> UIImage? {
         let rectTransform : CGAffineTransform
         switch (imageOrientation) {
         case .left:
@@ -36,5 +36,60 @@ extension UIImage {
         let imageRef: CGImage? = cgImage!.cropping(to: transformedCropSquare)
         
         return (imageRef != nil) ? UIImage(cgImage: imageRef!, scale: 1.0, orientation: imageOrientation): nil
+    }
+    
+    // MARK: Resize image
+    fileprivate func resize(with size: CGSize, transform: CGAffineTransform, drawTransposed: Bool, quality: CGInterpolationQuality) -> UIImage? {
+        let newRect = CGRect(origin: CGPoint.zero, size: size).integral
+        let transposedRect = CGRect(origin: CGPoint.zero, size: newRect.size)
+        guard let imageRef = cgImage else {
+            return nil
+        }
+        
+        let width = Int(newRect.size.width)
+        let height = Int(newRect.size.height)
+        let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: imageRef.bitsPerComponent, bytesPerRow: 0, space: imageRef.colorSpace!, bitmapInfo: imageRef.bitmapInfo.rawValue)
+        guard let bitmap = context else {
+            return nil
+        }
+        
+        bitmap.concatenate(transform)
+        bitmap.interpolationQuality = quality
+        bitmap.draw(imageRef, in: drawTransposed ? transposedRect : newRect)
+        
+        let newImageRef = bitmap.makeImage()
+        return newImageRef != nil ? UIImage(cgImage: newImageRef!) : nil
+    }
+    
+    func resize(with size: CGSize, interpolationQuality: CGInterpolationQuality = .high) -> UIImage? {
+        let drawTransposed = imageOrientation == .left || imageOrientation == .leftMirrored || imageOrientation == .right || imageOrientation == .rightMirrored
+        
+        let transform = transformForOrientation(size)
+        
+        return resize(with: size, transform: transform, drawTransposed: drawTransposed, quality: interpolationQuality)
+    }
+    
+    fileprivate func transformForOrientation(_ newSize: CGSize) -> CGAffineTransform {
+        var transform = CGAffineTransform.identity
+        if imageOrientation == .down || imageOrientation == .downMirrored {
+            transform = transform.translatedBy(x: newSize.width, y: newSize.height)
+            transform = transform.rotated(by: CGFloat(180).radians)
+        } else if imageOrientation == .left || imageOrientation == .leftMirrored {
+            transform = transform.translatedBy(x: newSize.width, y: 0)
+            transform = transform.rotated(by: CGFloat(90).radians)
+        } else if imageOrientation == .right || imageOrientation == .rightMirrored {
+            transform = transform.translatedBy(x: 0, y: newSize.height)
+            transform = transform.rotated(by: CGFloat(-90).radians)
+        }
+        
+        if  imageOrientation == .upMirrored || imageOrientation == .downMirrored {
+            transform = transform.translatedBy(x: newSize.width, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        } else if imageOrientation == .leftMirrored || imageOrientation == .rightMirrored {
+            transform = transform.translatedBy(x: newSize.height, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        }
+        
+        return transform
     }
 }

7f8820b4b25a8b49338aaced2bb42f8dd4c99fa9
noblakit01 <noblakit01@gmail.com>
2017-09-21 22:45:40 +0700

    add function create image from color

--- Sources/UIImage.swift
+++ Sources/UIImage.swift
@@ -6,4 +6,16 @@
 //
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    func image(with color: UIColor, size: CGSize) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(rect)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+}

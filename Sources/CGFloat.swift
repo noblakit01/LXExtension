e54d6010025aced204025835b1017eeb60bdeb64
noblakit01 <noblakit01@gmail.com>
2017-09-21 22:51:40 +0700

    Add extension CGFloat

---
+++ Sources/CGFloat.swift
@@ -0,0 +1,15 @@
+//
+//  CGFloat.swift
+//  LXExtension
+//
+//  Created by luan on 9/21/17.
+//
+//
+
+import CoreGraphics
+
+extension CGFloat {
+    var radians: CGFloat {
+        return self * CGFloat(Float.pi) / 180.0
+    }
+}

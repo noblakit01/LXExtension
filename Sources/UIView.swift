c732d8331c9555190dfbcf1c576b179587ad8295
Trần Minh Luận <noblakit01@gmail.com>
2017-10-17 14:53:17 +0700

    Update UIView.swift

--- Sources/UIView.swift
+++ Sources/UIView.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 extension UIView {
+    
     public class func fromNib<T : UIView>() -> T {
         return Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
     }

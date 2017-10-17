dc8e953c4d28070e2915ca62ab4d365953977e23
Trần Minh Luận <noblakit01@gmail.com>
2017-10-17 14:53:58 +0700

    Update LXExtensionTests.swift

--- Tests/LXExtensionTests/LXExtensionTests.swift
+++ Tests/LXExtensionTests/LXExtensionTests.swift
@@ -2,9 +2,9 @@ import XCTest
 @testable import LXExtension
 
 class LXExtensionTests: XCTestCase {
+    
     func testExample() {
     }
-
 
     static var allTests = [
         ("testExample", testExample),

b12352784f24771d252172f1aef4fa4dbcaf7f16
Luan Tran <noblakit01@gmail.com>
2017-12-05 09:22:22 +0700

    Add integer for degrees convert to radians

---
+++ Sources/BinaryInteger.swift
@@ -0,0 +1,16 @@
+//
+//  BinaryInteger.swift
+//  LXExtension
+//
+//  Created by Minh Luan Tran on 12/5/17.
+//
+
+import Foundation
+
+extension BinaryInteger {
+    
+    var radians: Float {
+        return Float(Int(self)) * .pi / 180
+    }
+
+}

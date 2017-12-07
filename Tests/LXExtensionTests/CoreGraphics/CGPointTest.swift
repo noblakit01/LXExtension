bfa88bd205f8d4661dc7d16199a75e613b9c81cd
Luan Tran <noblakit01@gmail.com>
2017-12-08 06:52:50 +0700

    Add CGPointTest

---
+++ Tests/LXExtensionTests/CoreGraphics/CGPointTest.swift
@@ -0,0 +1,23 @@
+//
+//  CGPointTest.swift
+//  LXExtensionTests
+//
+//  Created by Minh Luan Tran on 12/8/17.
+//
+
+import XCTest
+@testable import LXExtension
+
+class CGPointTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}

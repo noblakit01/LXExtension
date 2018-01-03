a77e967ff7480ad6a0398703df666d77d9a8f983
Luan Tran <noblakit01@gmail.com>
2018-01-03 21:05:48 +0700

    Add UIColorTest

---
+++ Tests/LXExtensionTests/UIKit/UIColorTest.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor.swift
+//  LXExtensionTests
+//
+//  Created by Minh Luan Tran on 1/3/18.
+//
+
+import XCTest
+@testable import LXExtension
+
+class UIColorTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}

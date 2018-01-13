0a1b864c86e428bca65d831d043995cd377a0e5d
Luan Tran <noblakit01@gmail.com>
2018-01-13 22:09:57 +0700

    Add extension star loading in view. Will return loadingview

--- Sources/UIKit/UIView.swift
+++ Sources/UIKit/UIView.swift
@@ -25,4 +25,37 @@ extension UIView {
             return String(describing: self)
         }
     }
+    
+    func startLoading() -> UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let dimmingView = UIView()
+        dimmingView.translatesAutoresizingMaskIntoConstraints = false
+        dimmingView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        
+        let indicatorView = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        indicatorView.startAnimating()
+        
+        view.addSubview(dimmingView)
+        view.addSubview(indicatorView)
+        
+        dimmingView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        dimmingView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        dimmingView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        dimmingView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        indicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        indicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        addSubview(view)
+        view.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        view.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        view.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        view.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        return view
+    }
+    
 }

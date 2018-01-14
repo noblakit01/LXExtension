950fe6ea646d3a2d7e8b98c8efc62e2bd7efd57f
Luan Tran <noblakit01@gmail.com>
2018-01-14 21:23:46 +0700

    Add function get width/height for String with constrainedHeight/Width

--- Sources/UIKit/UIFont.swift
+++ Sources/UIKit/UIFont.swift
@@ -5,4 +5,22 @@
 //  Created by Minh Luan Tran on 1/14/18.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    
+    func height(for string: String, withConstrainedWidth width: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = string.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: self], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    func width(for string: String, withConstrainedHeight height: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = string.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: self], context: nil)
+        
+        return ceil(boundingBox.width)
+    }
+    
+}

639b0b6d1a73d0c918f1f86fe33150ae8433920e
Luan Tran <noblakit01@gmail.com>
2018-12-27 00:10:52 +0700

    Add more scale function for CVPixelBuffer

--- Sources/CoreVideo/CVPixelBuffer.swift
+++ Sources/CoreVideo/CVPixelBuffer.swift
@@ -7,13 +7,116 @@
 
 import CoreVideo
 import Accelerate
+import CoreImage
 
 extension CVPixelBuffer {
-    
     var rect: CGRect {
         return CGRect(x: 0, y: 0,
                       width: CVPixelBufferGetWidth(self),
                       height: CVPixelBufferGetHeight(self))
+    }
+}
+
+extension CVPixelBuffer {
+    
+    public static func create(width: Int, height: Int) -> CVPixelBuffer? {
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(nil, width, height, kCVPixelFormatType_32BGRA, nil, &pixelBuffer)
+        if status != kCVReturnSuccess {
+            print("Error: Cannot create CVPixelBuffer with size (\(width), \(height)")
+            return nil
+        }
+        return pixelBuffer
+    }
+    
+}
+
+extension CVPixelBuffer {
+    
+    public static func resizePixelBuffer(_ srcPixelBuffer: CVPixelBuffer,
+                                         cropX: Int,
+                                         cropY: Int,
+                                         cropWidth: Int,
+                                         cropHeight: Int,
+                                         scaleWidth: Int,
+                                         scaleHeight: Int) -> CVPixelBuffer? {
+        let flags = CVPixelBufferLockFlags(rawValue: 0)
+        guard kCVReturnSuccess == CVPixelBufferLockBaseAddress(srcPixelBuffer, flags) else {
+            return nil
+        }
+        defer { CVPixelBufferUnlockBaseAddress(srcPixelBuffer, flags) }
+        
+        guard let srcData = CVPixelBufferGetBaseAddress(srcPixelBuffer) else {
+            print("Error: could not get pixel buffer base address")
+            return nil
+        }
+        let srcBytesPerRow = CVPixelBufferGetBytesPerRow(srcPixelBuffer)
+        let offset = cropY*srcBytesPerRow + cropX*4
+        var srcBuffer = vImage_Buffer(data: srcData.advanced(by: offset),
+                                      height: vImagePixelCount(cropHeight),
+                                      width: vImagePixelCount(cropWidth),
+                                      rowBytes: srcBytesPerRow)
+        
+        let destBytesPerRow = scaleWidth*4
+        guard let destData = malloc(scaleHeight*destBytesPerRow) else {
+            print("Error: out of memory")
+            return nil
+        }
+        var destBuffer = vImage_Buffer(data: destData,
+                                       height: vImagePixelCount(scaleHeight),
+                                       width: vImagePixelCount(scaleWidth),
+                                       rowBytes: destBytesPerRow)
+        
+        let error = vImageScale_ARGB8888(&srcBuffer, &destBuffer, nil, vImage_Flags(0))
+        if error != kvImageNoError {
+            print("Error:", error)
+            free(destData)
+            return nil
+        }
+        
+        let releaseCallback: CVPixelBufferReleaseBytesCallback = { _, ptr in
+            if let ptr = ptr {
+                free(UnsafeMutableRawPointer(mutating: ptr))
+            }
+        }
+        
+        let pixelFormat = CVPixelBufferGetPixelFormatType(srcPixelBuffer)
+        var dstPixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreateWithBytes(nil, scaleWidth, scaleHeight,
+                                                  pixelFormat, destData,
+                                                  destBytesPerRow, releaseCallback,
+                                                  nil, nil, &dstPixelBuffer)
+        if status != kCVReturnSuccess {
+            print("Error: could not create new pixel buffer")
+            free(destData)
+            return nil
+        }
+        return dstPixelBuffer
+    }
+    
+    /**
+     Resizes a CVPixelBuffer to a new width and height.
+     */
+    public static func resizePixelBuffer(_ pixelBuffer: CVPixelBuffer,
+                                  width: Int, height: Int) -> CVPixelBuffer? {
+        return resizePixelBuffer(pixelBuffer, cropX: 0, cropY: 0,
+                                 cropWidth: CVPixelBufferGetWidth(pixelBuffer),
+                                 cropHeight: CVPixelBufferGetHeight(pixelBuffer),
+                                 scaleWidth: width, scaleHeight: height)
+    }
+    
+    public func resize(to size: CGSize, context: CIContext = CIContext()) -> CVPixelBuffer? {
+        guard let outputBuffer = CVPixelBuffer.create(width: Int(size.width), height: Int(size.height)) else {
+            return nil
+        }
+        
+        let ciImage = CIImage(cvPixelBuffer: self)
+        let sx = size.width / CGFloat(CVPixelBufferGetWidth(self))
+        let sy = size.height / CGFloat(CVPixelBufferGetHeight(self))
+        let scaleTransform = CGAffineTransform(scaleX: sx, y: sy)
+        let scaledImage = ciImage.transformed(by: scaleTransform)
+        context.render(scaledImage, to: outputBuffer)
+        return outputBuffer
     }
     
     public func resize_vImage(to size: CGSize) -> CVPixelBuffer {

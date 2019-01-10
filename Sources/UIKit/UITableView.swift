2a412595bf76f11053c74204516ec76a23b01580
Luan Tran <noblakit01@gmail.com>
2019-01-10 13:31:54 +0700

    add function for dequeue cell with UITableViewCell registed

--- Sources/UIKit/UITableView.swift
+++ Sources/UIKit/UITableView.swift
@@ -10,14 +10,25 @@ import UIKit
 
 extension UITableView {
     
-    public func register(cellType: UITableViewCell.Type) {
-        let nib = cellType.nib
-        register(nib, forCellReuseIdentifier: cellType.identifier)
+    public func registerClass(of cell: UITableViewCell.Type) {
+        register(cell, forCellReuseIdentifier: cell.identifier)
+    }
+    
+    public func registerNib(of cell: UITableViewCell.Type) {
+        let nib = cell.nib
+        register(nib, forCellReuseIdentifier: cell.identifier)
     }
     
     public func registerHeaderFooterView(_ view: UIView.Type) {
         let nib = view.nib
         register(nib, forHeaderFooterViewReuseIdentifier: view.identifier)
+    }
+    
+    public func dequeueReusableCell<T: UITableViewCell>(cellClass: T.Type, for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: cellClass.identifier, for: indexPath) as? T else {
+            fatalError("Can't dequeue reusable cell with \(T.description())")
+        }
+        return cell
     }
     
 }

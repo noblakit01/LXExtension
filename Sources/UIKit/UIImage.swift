f850ef2a187766635a45358273de958f776b9d88
Luan Tran <noblakit01@gmail.com>
2019-01-16 11:08:50 +0700

    Change function init UIImage with color & size to static

--- Sources/UIKit/UIImage.swift
+++ Sources/UIKit/UIImage.swift
@@ -35,7 +35,7 @@ extension UIImage {
         }
     }
     
-    public func image(with color: UIColor, size: CGSize) -> UIImage {
+    public static func image(with color: UIColor, size: CGSize) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
         color.setFill()

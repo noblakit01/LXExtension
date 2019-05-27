713d94b7f46b7d1c41bdc9ed35495a58f47badce
Luan Tran <noblakit01@gmail.com>
2019-05-27 15:56:04 +0700

    Add extension UIEdgeInsets init with only 1 paddings

---
+++ lxextension/lxextension/UIKit/UIEdgeInsets.swift
@@ -0,0 +1,17 @@
+//
+//  UIEdgeInsets.swift
+//  lxextension
+//
+//  Created by luantran on 5/27/19.
+//  Copyright © 2019 luantran. All rights reserved.
+//
+
+import UIKit
+
+extension UIEdgeInsets {
+    
+    public init(padding: CGFloat) {
+        self.init(top: padding, left: padding, bottom: padding, right: padding)
+    }
+    
+}

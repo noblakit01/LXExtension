17f1e4643286f82861918a5156d36bcfa3381a86
Luan Tran <noblakit01@gmail.com>
2019-07-04 15:14:18 +0700

    set inset, padding of UIButtons

--- lxextension/lxextension/UIKit/UIButton+Property.swift
+++ lxextension/lxextension/UIKit/UIButton+Property.swift
@@ -22,6 +22,23 @@ public extension UIButton {
     }
     
     @discardableResult
+    func setInsets(for contentPadding: UIEdgeInsets, imageTitlePadding: CGFloat) -> UIButton {
+        self.contentEdgeInsets = UIEdgeInsets(
+            top: contentPadding.top,
+            left: contentPadding.left,
+            bottom: contentPadding.bottom,
+            right: contentPadding.right + imageTitlePadding
+        )
+        self.titleEdgeInsets = UIEdgeInsets(
+            top: 0,
+            left: imageTitlePadding,
+            bottom: 0,
+            right: -imageTitlePadding
+        )
+        return self
+    }
+    
+    @discardableResult
     func set(_ font: UIFont) -> UIButton {
         titleLabel?.setFont(font)
         return self

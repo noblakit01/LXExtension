f4243697ddefcf66b4a27114f430d23bb278fa38
Luan Tran <noblakit01@gmail.com>
2020-12-29 23:49:13 +0700

    Add hhMMss string for timeinterval

--- lxextension/lxextension/Foundation/TimeInterval.swift
+++ lxextension/lxextension/Foundation/TimeInterval.swift
@@ -18,4 +18,17 @@ public extension TimeInterval {
         self.init(second)
     }
     
+    var hhMMss: String {
+        let secondsInt = Int(self)
+        let h = secondsInt / 3600
+        let remainS = secondsInt - h * 3600
+        let m = remainS / 60
+        let s = remainS - m * 60
+        if h > 0 {
+            return String(format: "%02d:%02d:%02d", h, m, s)
+        } else {
+            return String(format: "%02d:%02d", m, s)
+        }
+    }
+    
 }
